7db17a1b6f9b5e6b0899332e2fcf9cb880a23c03
karadagserhat <serhatkaradagg@outlook.com>
2023-08-18 15:39:07 +0300

    first commit

---
+++ src/assets/wrappers/CocktailCard.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+const Wrapper = styled.article`
+  background: var(--white);
+  box-shadow: var(--shadow-2);
+  transition: var(--transition);
+  display: grid;
+  grid-template-rows: auto 1fr;
+  border-radius: var(--borderRadius);
+  :hover {
+    box-shadow: var(--shadow-4);
+  }
+  img {
+    height: 15rem;
+    border-top-left-radius: var(--borderRadius);
+    border-top-right-radius: var(--borderRadius);
+    filter: blur(20px);
+    /* filter: opacity(0); */
+  }
+  .footer {
+    padding: 1.5rem;
+    h4,
+    h5 {
+      margin-bottom: 0.5rem;
+    }
+    h4 {
+      font-weight: 700;
+    }
+    p {
+      margin-bottom: 1rem;
+      color: var(--grey-500);
+    }
+  }
+`;
+
+export default Wrapper;
